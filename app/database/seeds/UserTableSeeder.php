ac47b8d083190bc86e0a7b6fdfb07eec071156bf
Stephanie <sriera3@gmail.com>
2015-02-06 11:02:40 -0600

    Updated user table seeder to use ENV variables

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -5,8 +5,8 @@ class UserTableSeeder extends Seeder {
 	public function run() {
 
 		$user = new User();
-		$user->email = $_ENV['DEFAULT_EMAIL'];
-		$user->password = $_ENV['DEFAULT_PASS'];
+		$user->email = $_ENV['DEFAULT_USER_EMAIL'];
+		$user->password = $_ENV['DEFAULT_USER_PASSWORD'];
 		$user->save();
 
 	}
